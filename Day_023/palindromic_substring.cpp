c4fe817db37e5b71702c590a0dea938b9d6d9b3e
CoderJap <japjotwork007@gmail.com>
2026-03-21 23:53:09 +0530

    Day 23 progressa added

---
+++ Day_023/palindromic_substring.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int expand(string &s , int left , int right){
+        int count = 0;
+        while(left >= 0 && right < s.length() && s[left] == s[right]){
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int total = 0;
+
+        for(int i=0;i<s.length();i++){
+
+        total += expand(s,i,i); // odd
+        total += expand(s,i,i+1); // even
+        }
+
+        return total;
+    }
+};
