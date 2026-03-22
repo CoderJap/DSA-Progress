eb4969a1cc51f1f141406bcd59a5183567ca5755
CoderJap <japjotwork007@gmail.com>
2026-03-22 23:41:57 +0530

    problem 1 solved

---
+++ Day_024/equivalent_keypad_sequence.cpp
@@ -0,0 +1,30 @@
+
+
+
+string printSequence(string S) {
+    
+    vector <string> keypad = {
+        "2" , "22" , "222" , // A B C
+        "3" , "33" , "333" , // D E F
+        "4" , "44" , "444" , // G H I 
+        "5" , "55" , "555" , // J K L 
+        "6" , "66" , "666" , // M N O
+        "7" , "77" , "777" , "7777" , // P Q R S    
+        "8" , "88" , "888" , // T U V
+        "9" , "99" , "999" , "9999" , // W X Y Z
+    };
+    
+    string result = "";
+    
+    for(char ch: S) {
+        if(ch ==' '){
+            result+="0";
+        }
+        else {
+            result += keypad[ch-'A'];
+        }
+    }
+    
+    return result;
+    
+}
