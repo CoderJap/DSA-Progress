0f4e8bd6e30094ced1a528e8aa7baac2298c7fb6
Japjot Singh Nanda <143312889+CoderJap@users.noreply.github.com>
2026-03-24 23:01:25 +0530

    Day 26 progress added

---
+++ Day_026/reverse_number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int result = 0;
+
+        while(x!=0){
+            int digit = x%10;
+            x= x/10;
+
+            // overflow cond
+            if(result > INT_MAX/10 || result < INT_MIN/10)  return 0;
+
+            result = result * 10 + digit;
+
+        }
+
+        return result;
+        
+    }
+};
