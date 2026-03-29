4f0f7bc98faae5a31841e61a08daa8e997faa986
CoderJap <japjotwork007@gmail.com>
2026-03-29 22:34:46 +0530

    Day 31 progress added

---
+++ Day_31/check_string_equal_opr_1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+
+        // even indices(0 and 2)
+        string s1_even = {s1[0] , s1[2]};
+        string s2_even = {s2[0] , s2[2]};
+
+        // odd indices(1 and 3)
+        string s1_odd = {s1[1] , s1[3]};
+        string s2_odd = {s2[1] , s2[3]};
+
+        // sort them
+
+        sort(s1_even.begin() , s1_even.end());
+        sort(s2_even.begin() , s2_even.end());
+
+        sort(s1_odd.begin() , s1_odd.end());
+        sort(s2_odd.begin() , s2_odd.end());
+
+        return (s1_even == s2_even) && (s1_odd == s2_odd);
+        
+    }
+};
